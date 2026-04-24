3b2d8a02f0100152ade7092d09bd77666e6533b4
Touiku411 <tengyu081229@gmail.com>
2026-04-24 18:04:43 +0800

    ,

---
+++ sddm/install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+
+CURRENT_DIR=$HOME/arch-hyprland
+rm -rf SilentSDDM
+git clone -b main --depth=1 https://github.com/uiriansan/SilentSDDM && cd SilentSDDM && ./install.sh
+
+sudo mkdir -p /etc/sddm.conf.d
+
+sudo tee /etc/sddm.conf.d/theme.conf > /dev/null <<EOF
+[General]
+InputMethod=qtvirtualkeyboard
+GreeterEnvironment=QML2_IMPORT_PATH=/usr/share/sddm/themes/silent/components/,QT_IM_MODULE=qtvirtualkeyboard
+
+[Theme]
+Current=silent
+EOF
+
+sudo systemctl enable -f sddm.service
+
+echo -e "\n--- 目前的 SDDM 設定檔內容 ---"
+cat /etc/sddm.conf.d/theme.conf
+
+echo -e "\ninstall successful"
+read -p "test? [Y/n]: " CONFIRM
+CONFIRM=${CONFIRM,,}
+if [[ $CONFIRM == "yes" || $CONFIRM == "y" || -z $CONFIRM ]]; then
+    cd "${CURRENT_DIR}/sddm" && ./test.sh
+fi
+
+
