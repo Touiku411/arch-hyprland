d6ce4cbe6cbdfbe544379fa752bddecf3d5a3163
Touiku411 <tengyu081229@gmail.com>
2026-04-25 01:57:43 +0800

    .

---
+++ setup.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+set -e
+DOT_DIR="$HOME/arch-hyprland"
+
+PKGS_PACMAN=(
+    "hyprland"
+    "kitty"
+    "waybar"
+    "rofi-wayland"
+    "swaync"       
+    "cava"     
+    "fastfetch"
+    "matugen"
+    "git" "base-devel"
+    "ttf-jetbrains-mono-nerd"
+)
+PKGS_AUR=(
+    "awww"
+    "wlogout"
+    "waypaper"
+    "visual-studio-code-bin"
+)
+
+sudo pacman -Syu --noconfirm
+
+echo "install pacman pkgs..."
+sudo pacman -S --noconfirm "${PKGS_PACMAN[@]}"
+
+if ! command -v yay &> /dev/null; then
+    echo "install yay..."
+    cd /tmp
+    git clone https://aur.archlinux.org/yay.git
+    cd yay
+    makepkg -si --noconfirm
+    cd "$DOT_DIR"
+fi
+
+echo "install yay pkgs..."
+yay -S --noconfirm "${PKGS_AUR[@]}"
+
+echo "link..."
+mkdir -p ~/.config
+CONFIG_APPS=("hypr" "kitty" "waybar" "rofi" "swaync" "wlogout" "cava" "fastfetch" "matugen")
+
+
+for app in "${CONFIG_APPS[@]}"; do
+    if [ -d "$DOT_DIR/.config/$app" ]; then
+        rm -rf "$HOME/.config/$app"
+        ln -snf "$DOT_DIR/.config/$app" "$HOME/.config/$app"
+        echo "link $app"
+    else
+        echo "can not find $DOT_DIR/.config/$app... skip..."
+    fi
+done
+
+sudo ln -snf /usr/bin/awww /usr/bin/swww
+
+echo "install sddm...."
+cd "${DOT_DIR}/sddm"
+chmod +x install.sh && ./install.sh
